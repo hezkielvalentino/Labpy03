29e5e2c674d5a0d88222ed712f3078efc0176b2b
hezkielvalentino <93851727+hezkielvalentino@users.noreply.github.com>
2021-11-07 14:08:17 +0700

    Add files via upload

---
+++ Latihan2.py
@@ -0,0 +1,20 @@
+
+print(' ')
+print('-----Program Menampilkan bilangan terbesar-----')
+print(' ')
+
+
+max = 0
+
+
+while True:
+    a = int(input('Masukkan bilangan:'))
+    if a >= max:
+        max = a
+    if a == 0:
+        break
+
+
+print('Nilai terbesarnya adalah :',max)
+print(' ')
+print('--------------------SELESAI--------------------')
